d649f0bf2c64e3303e1c9ccfc9a9f4888bd2592f
FARAZ AHMAD <shahid@phptravels.org>
2026-09-11 21:14:09 +0500

    Update portfolio for Shahid Iqbal and set remote to shahidiqbal4261-cpu/portfolio1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/portfolio1/",
   plugins: [react(), tailwindcss()],
   server: {
     open: true,
